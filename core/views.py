344ecc5471d05581e170dee10937e6567aab9564
mack66hm <mack66hm@gmail.com>
2020-02-24 16:51:12 -0500

    updated notes app and got homepage working

--- core/views.py
+++ core/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+import data 
 
 # Create your views here.
+def index(request):
+    # return HttpResponse("Hello, lets get started.")
+    notes = data.NOTES
+    return render(request, 'base.html', {'notes': notes})
