6c9c3b0c466a68571062162c8c8424bce49db6e1
mack66hm <mack66hm@gmail.com>
2020-02-25 11:39:56 -0500

    created admin page and models for ntoes

--- core/views.py
+++ core/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-import data 
+from .models import Note
 
 # Create your views here.
-def index(request):
+def notes_list(request):
     # return HttpResponse("Hello, lets get started.")
-    notes = data.NOTES
-    return render(request, 'base.html', {'notes': notes})
+    notes = Note.objects.all()
+    return render(request, 'core/notes_list.html', {'notes': notes})
+
+def notes_detail(request, pk):
+    note = Note.objects.get(pk=pk)
+    return render(request, 'core/notes.detail.html', {'note' : note})
